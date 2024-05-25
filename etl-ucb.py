2152f96c50864b087f293f91f4ab702e29fd4bad
EstebanDeLaVegaLang <47247534+EstebanDeLaVegaLang@users.noreply.github.com>
2024-05-25 00:12:05 +0000

    Loading

--- etl-ucb.py
+++ etl-ucb.py
@@ -12,3 +12,11 @@ wine_quality_data = pd.read_csv(wine_quality_url, sep = ";")
 print(wine_data.head())
 print(wine_quality_data.head())
 
+# Tansformacion
+
+
+
+# Loading
+# Saving the transformed data as csv file
+wine_data.to_csv("wine_dataset.csv", index = False)
+wine_quality_data.to_csv("wine_quality_dataset.csv",  index = False)
