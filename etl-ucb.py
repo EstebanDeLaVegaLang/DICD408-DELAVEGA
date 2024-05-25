2c7a53462636d1765f1098e9014d8b4b95e19674
EstebanDeLaVegaLang <47247534+EstebanDeLaVegaLang@users.noreply.github.com>
2024-05-25 01:08:38 +0000

    Normalizing "alcohol"

--- etl-ucb.py
+++ etl-ucb.py
@@ -9,14 +9,31 @@ wine_quality_url = "http://archive.ics.uci.edu/ml/machine-learning-databases/win
 wine_quality_data = pd.read_csv(wine_quality_url, sep = ";")
 
 # Initial look of data
-print(wine_data.head())
-print(wine_quality_data.head())
+#print(wine_data.head())
+#print(wine_quality_data.head())
 
 # Tansformacion
+# Agregando nombres a columnas
+wine_data.columns = ["class", "alcohol", "malic acid", "ash", 
+                    "alcalinity of ash", "magnesium", "total phenols", 
+                    "flavonoids", "nonflavonoind phenols", "proanthocyanidins", 
+                    "color intensity", "hue", "OD280/OD315 of diluted wines", 
+                    "proline"]
 
+# Converting 
+wine_data["class"] = wine_data["class"].astype("category")
+
+# Checking for any missing values in both datasets
+#print(wine_data.isnull().sum())
+#print(wine_quality_data.isnull().sum())
+
+# Normalizing "alcohol" column in the wine_data using Min-Max normalization
+wine_data["alcohol"] =  (wine_data["alcohol"] - wine_data["alcohol"].min())/(wine_data["alcohol"].max() - wine_data["alcohol"].min())
 
 
 # Loading
 # Saving the transformed data as csv file
 wine_data.to_csv("wine_dataset.csv", index = False)
 wine_quality_data.to_csv("wine_quality_dataset.csv",  index = False)
+
+
---
+++ etl-ucb2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np 
+
+# Extraccion
+wine_url = "amazon_prime_users.csv"
+wine_data = pd.read_csv(wine_url, header = None)
+
+# Initial look of data
+print(wine_data.head())
+print(wine_data.shape)
+# Tansformacion
+
+
+
+# Loading
+# Saving the transformed data as csv file
+#wine_data.to_csv("wine_dataset.csv", index = False)
+
+
