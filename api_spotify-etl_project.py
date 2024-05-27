33d360b617e66cfe6cd406fdab8286acb45f80a8
EstebanDeLaVegaLang <47247534+EstebanDeLaVegaLang@users.noreply.github.com>
2024-05-27 05:28:38 +0000

    Spotify API - ETL con Python

---
+++ api_spotify-etl_project.py
@@ -0,0 +1,10 @@
+import sqlalchemy 
+import pandas as pd
+from sqlalchemy_orm import session
+import requests
+import json
+from datetime import datetime
+import datetime
+import sqlite3
+
+# database_location = ""
